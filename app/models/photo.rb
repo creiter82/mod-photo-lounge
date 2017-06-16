1afe536b287a4e3db5bca5b0a1f828a08512e640
Cameron Reiter <reiter303@hotmail.com>
2017-06-16 12:03:21 -0600

    Implement carrierwave for photo main_image

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,8 @@
 class Photo < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  mount_uploader :main_image, PhotoUploader
+  mount_uploaders :event_images, PhotoUploader
+
 end
